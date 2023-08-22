37fbbfbbc961d94b4d6d8224dee5a2d4594d7c52
chenxiaoman <chenxiaoman@bancheng.cn>
2023-08-22 08:00:19 +0800

    patch

--- preProcesser.py
+++ preProcesser.py
@@ -19,6 +19,62 @@ import torch
 #   "image_path"
 #)
 
+#https://github.com/patrickvonplaten/controlnet_aux
+#https://github.com/invoke-ai/InvokeAI/blob/0909812c84570d7998bfd90ebd136f252650a909/invokeai/app/invocations/controlnet_image_processors.py#L232
+#不需要自己封装，直接使用上面的即可，已经有人封装好了
+#用法
+"""
+from PIL import Image
+import requests
+from io import BytesIO
+from controlnet_aux import HEDdetector, MidasDetector, MLSDdetector, OpenposeDetector, PidiNetDetector, NormalBaeDetector, LineartDetector, LineartAnimeDetector, CannyDetector, ContentShuffleDetector, ZoeDetector, MediapipeFaceDetector, SamDetector, LeresDetector
+
+# load image
+url = "https://huggingface.co/lllyasviel/sd-controlnet-openpose/resolve/main/images/pose.png"
+
+response = requests.get(url)
+img = Image.open(BytesIO(response.content)).convert("RGB").resize((512, 512))
+
+# load checkpoints
+hed = HEDdetector.from_pretrained("lllyasviel/Annotators")
+midas = MidasDetector.from_pretrained("lllyasviel/Annotators")
+mlsd = MLSDdetector.from_pretrained("lllyasviel/Annotators")
+open_pose = OpenposeDetector.from_pretrained("lllyasviel/Annotators")
+pidi = PidiNetDetector.from_pretrained("lllyasviel/Annotators")
+normal_bae = NormalBaeDetector.from_pretrained("lllyasviel/Annotators")
+lineart = LineartDetector.from_pretrained("lllyasviel/Annotators")
+lineart_anime = LineartAnimeDetector.from_pretrained("lllyasviel/Annotators")
+zoe = ZoeDetector.from_pretrained("lllyasviel/Annotators")
+sam = SamDetector.from_pretrained("ybelkada/segment-anything", subfolder="checkpoints")
+mobile_sam = SamDetector.from_pretrained("dhkim2810/MobileSAM", model_type="vit_t", filename="mobile_sam.pt")
+leres = LeresDetector.from_pretrained("lllyasviel/Annotators")
+
+# instantiate
+canny = CannyDetector()
+content = ContentShuffleDetector()
+face_detector = MediapipeFaceDetector()
+
+
+# process
+processed_image_hed = hed(img)
+processed_image_midas = midas(img)
+processed_image_mlsd = mlsd(img)
+processed_image_open_pose = open_pose(img, hand_and_face=True)
+processed_image_pidi = pidi(img, safe=True)
+processed_image_normal_bae = normal_bae(img)
+processed_image_lineart = lineart(img, coarse=True)
+processed_image_lineart_anime = lineart_anime(img)
+processed_image_zoe = zoe(img)
+processed_image_sam = sam(img)
+processed_image_leres = leres(img)
+
+processed_image_canny = canny(img)
+processed_image_content = content(img)
+processed_image_mediapipe_face = face_detector(img)
+
+coarse：是否使用简易模式
+"""
+
 def getCannyImage(image, low_threshold: int = 100, high_threshold: int = 200):
     """
     获取canny图片，用于传入controlnet的canny模型
